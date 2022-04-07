d69d5f5c0081f441ef9dd1ce42ece4cba368c4ac
yussems <yussems@gmail.com>
2022-04-08 02:15:50 +0300

    finished project

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 const p = document.querySelectorAll("#paraf");
-const span = document.querySelector(".bottom-card-paraf");
+const spans = document.querySelectorAll(".bottom-card-paraf");
 
-p.forEach((item) => {
-  item.style.display = "none";
-});
-
-let state = false
-span.addEventListener("click", () => {
-    p.forEach(item => {
-        state = true
-        item.style.display = 'inline'
-    })
-    if(state) {
-        span.style.color = 'grey'
-        span.innerHTML = 'Daha Az'
-
-    }
-    
-  
+spans.forEach((span) => {
+  span.addEventListener("click", () => {
+    p.forEach((item, i) => {
+      if (span.id == i) {
+        let isRead = item.classList.toggle("toggle");
+        if (isRead) {
+          return (span.innerHTML = "Daha Fazla");
+        }
+        return (span.innerHTML = "Daha Az");
+      }
+    });
+  });
 });
